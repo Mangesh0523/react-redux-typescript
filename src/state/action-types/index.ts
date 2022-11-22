f4a9a3d81a7a71c1ea11b69c7dab486be424b1b1
mangesSh <bhaganagaremangesh@gmail.com>
2022-11-22 16:52:10 +0530

    bankapp initial code

---
+++ src/state/action-types/index.ts
@@ -0,0 +1,8 @@
+// basically enum is one of the specified things put into the enum
+
+export enum ActionType{
+DEPOSIT= "deposite",
+WITHDRAW= "withdraw",
+BANKRUPT = "bankrupt",
+
+}
---
+++ src/state/store.ts
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux'
+import reducers from './reducers/index'
+import thunk from 'redux-thunk'
+
+
+export const Store= createStore(
+    reducers,
+   {},
+    applyMiddleware(thunk)
+)
